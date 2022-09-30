ea14e6dcf8c876cfdb1f4efe110b77468d2674e7
Antoine Delattre <antoine.delattre@epita.fr>
2022-09-30 22:38:50 +0200

    Added base engine

---
+++ src/shader/pbr.frag.ts
@@ -0,0 +1,47 @@
+export default `
+precision highp float;
+
+
+in vec3 vNormalWS;
+out vec4 outFragColor;
+
+struct Material
+{
+  vec3 albedo;
+};
+
+struct Light
+{
+  vec3 position;
+  /* vec3 color; */
+};
+
+uniform Material uMaterial;
+uniform Light uLight; 
+
+// From three.js
+vec4 sRGBToLinear( in vec4 value ) {
+	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
+}
+
+// From three.js
+vec4 LinearTosRGB( in vec4 value ) {
+	return vec4( mix( pow( value.rgb, vec3( 0.41666 ) ) * 1.055 - vec3( 0.055 ), value.rgb * 12.92, vec3( lessThanEqual( value.rgb, vec3( 0.0031308 ) ) ) ), value.a );
+}
+
+void
+main()
+{
+  // **DO NOT** forget to do all your computation in linear space.
+  vec3 albedo = sRGBToLinear(vec4(uMaterial.albedo, 1.0)).rgb;
+
+  // **DO NOT** forget to apply gamma correction as last step.
+
+  //vec3 diffuse = ;
+
+  outFragColor.rgba = LinearTosRGB(vec4(albedo, 1.0));
+  
+  // To debug normals
+  /* outFragColor.rgba = LinearTosRGB(vec4(vNormalWS, 1.0)); */
+}
+`;
